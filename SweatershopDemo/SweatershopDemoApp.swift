b0a32447d69196046b0608645383a3b10277f0cc
Alandis <1seals.alandis@gmail.com>
2022-07-20 17:17:25 -0500

    initial commit

---
+++ SweatershopDemo/SweatershopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweatershopDemoApp.swift
+//  SweatershopDemo
+//
+//  Created by Alandis Seals on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SweatershopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
